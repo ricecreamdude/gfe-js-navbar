30e4d2aa4c5da4872f6b0658106e7df9af01f506
Joshua Ho <ho.joshua4@gmail.com>
2024-07-11 00:06:07 -0700

    Added menu items using ul / li

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
+
+
+/** 
+ * 
+ * We need a media query in JAVASCRIPT that when 1280 is reached
+ * -    Hide the hamburger icon  
+ * -    Show the nav bar 
+*/
+//
+
+
+
+
+const handleScreenSizeChange = () => {
+    
+}
